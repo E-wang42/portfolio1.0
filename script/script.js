205936552b0b25a2acb202fa03f91331a63399c0
eric <ericwang42@outlook.com>
2022-07-14 23:24:41 -0700

    update

---
+++ script/script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const x = "hello there";
+
+(function () {
+  return console.log(x);
+})();
+
+function rolled() {
+  if (true) {
+    const x = `${rick}`;
+    const y = "never gonna let you down";
+    console.log(x + y);
+    const rick = "never gonna give you up";
+  }
+}
